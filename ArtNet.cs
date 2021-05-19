d7f5fa1550fb5ee56bae55c39b9d8319cb0e6876
Денис Шишмарев <denisshishmariov@gmail.com>
2021-05-19 15:33:32 +0300

    Добавьте файлы проекта.

---
+++ ArtNet.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+
+namespace UDPTestToolArcNet
+{
+    class ArtNet
+    {
+        UdpClient udpClient, receiver;
+        const string                           
+            ARTNETHEADER = "4172742d4e657400", 
+            ARTNET_OP_CODE_DMX_SET = "0050",
+            ARTNET_OP_CODE_TOD_REQUEST = "0080",
+            ARTNET_ARTTOD_REQ_PACKET =  "0000000000000000000000010100000000000000000000000000000000000000000000000000000000000000",
+            ARTNET_ARTTOD_REQ_PACKET2=   "0000000000000000000000000100000000000000000000000000000000000000000000000000000000000000",
+            ARTNET_OP_CODE_CONTROL_PACKET = "0082",
+            ARTNET_ARTTOD_CONTROL_PACKET = "000000000000000000000101",
+            ARTNET_PROT_VERSION = "000e",
+            ARTNET_PHYSICAL = "00", 
+            HEX_FORMAT_2_BYTES = "D2", 
+            HEX_FORMAT_4_BYTES = "D4",
+            ARTNET_LENGHT_BYTE0 = "02",
+            ARTNET_LENGHT_BYTE1 = "00",
+            ARTNET_UNIVERSE = "0100";
+        static byte _sequenceCounter = 0;
+        string _packet = "";
+        const int CHANNEL_NUMBER = 512, CHANNEL_BYTES_NUMBER = 2;
+        byte[] dataBytes = null;
+        public string message;
+        public ArtNet(int port) 
+        {
+                        
+        }
+              
+        public void ConnectToDevice(string ip, int port) 
+        {
+            IPEndPoint localpt = new IPEndPoint(IPAddress.Any, 6454);
+            udpClient = new UdpClient(ip,port);
+            //udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Connect(ip, port);
+
+            //receiver = new UdpClient();
+            //udpClient.EnableBroadcast = true;
+            //receiver.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //receiver.Client.Bind(localpt);
+        }
+        
+        //Формирование и отправка DMX сообщения для управления светом. 
+        public void SendDMX(byte[] data)
+        {
+            _sequenceCounter++;
+            string sequence = _sequenceCounter.ToString(HEX_FORMAT_2_BYTES);
+            //Формирование АртНет составляющей пакета
+            _packet = ARTNETHEADER + ARTNET_OP_CODE_DMX_SET + ARTNET_PROT_VERSION + sequence + ARTNET_PHYSICAL + ARTNET_UNIVERSE + ARTNET_LENGHT_BYTE0 + ARTNET_LENGHT_BYTE1;
+           
+            byte[] bytes = StringToByteArray(_packet);
+            byte[] query = new byte[bytes.Length + CHANNEL_NUMBER];
+
+            bytes.CopyTo(query, 0);
+            data.CopyTo(query, bytes.Length);
+
+            try
+            {
+                udpClient.Send(query, query.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length / 2).Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16)).ToArray();            
+        }
+
+        private void DiscoverySender(string header) {
+            byte[] bytes = StringToByteArray(header);
+            try
+            {
+                udpClient.Send(bytes, bytes.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }           
+        }
+        public void Discover() {
+            //Формирование АртНет пакета
+            DiscoverySender(ARTNETHEADER + ARTNET_OP_CODE_TOD_REQUEST + ARTNET_PROT_VERSION + ARTNET_ARTTOD_REQ_PACKET);
+            DiscoverySender(ARTNETHEADER + ARTNET_OP_CODE_CONTROL_PACKET + ARTNET_PROT_VERSION + ARTNET_ARTTOD_CONTROL_PACKET);            
+        }
+        IPEndPoint remoteIp = null;
+        public void ReceiveMessage()
+        {
+
+            //new IPEndPoint(IPAddress.Parse("2.188.104.63"), 6454); // адрес входящего подключения
+            try
+            {
+                dataBytes = udpClient.Receive(ref remoteIp);// (ref remoteIp); // получаем данные
+                //message = Encoding.Unicode.GetString(dataBytes);
+               // MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "Вот это сообщение");
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+            if (dataBytes == null)
+            {
+                dataBytes = new byte[] { 0x00 };
+            }
+            message = BitConverter.ToString(dataBytes);
+            //return message;
+           
+        }
+        public void Close()
+        {
+            udpClient.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UDPTestToolArcNet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArtNet udpConnection;
+        const int CHANNEL_NUMBER = 512, DEFAULT_PORT = 6454;
+        const string DEFAULT_IP = "2.188.104.63";
+        byte[] _dmxData = new byte[CHANNEL_NUMBER], discoveryData;
+        int _port;
+        string _ip;
+        Task receiveTask;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.Closed += MainWindow_Closed;
+            
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            udpConnection.Close();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            udpConnection = new ArtNet(DEFAULT_PORT);
+            udpConnection.ConnectToDevice(DEFAULT_IP, DEFAULT_PORT);
+            Label_IP.Content = DEFAULT_IP;
+            Label_PORT.Content = DEFAULT_PORT;
+            TextBox_IP.Text = DEFAULT_IP;            
+            TextBox_Port.Text = DEFAULT_PORT.ToString();
+            receiveTask = new Task(udpConnection.ReceiveMessage);
+            receiveTask.Start();
+        }
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {            
+            byte value;            
+            try
+            {
+                value = Convert.ToByte(TextBox_Value.Text);                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат количества каналов или значения каналов");
+                return;
+            }            
+            for (int i = 0; i < CHANNEL_NUMBER; i++)
+            {
+                _dmxData[i] = value;
+            }
+            udpConnection.SendDMX(_dmxData);
+            FillDataGrid(_dmxData);
+        }
+
+       
+        private void FillDataGrid(byte[] values) 
+        {
+            DataTable dt = new DataTable();
+
+            string header = "";
+
+            dt.Columns.Add("№x10", typeof(string));
+
+            const int ROWS_NUMBER = 52, COLUMNS_NUMBER = 10;
+            for (int i = 0; i < ROWS_NUMBER; i++)
+            {                
+                dt.Rows.Add();
+            }
+            for (int i = 0; i < COLUMNS_NUMBER+1; i++)
+            {
+                header = $"{i}";
+                dt.Columns.Add(header, typeof(byte));
+            }
+            
+            int index = 0;
+            for (int rowId = 0; rowId < ROWS_NUMBER; rowId++)
+            {
+                dt.Rows[rowId][0] = $"{rowId}0";
+                for (int columnId = 1; columnId < COLUMNS_NUMBER; columnId++)
+                {                   
+                    if (index >= values.Length)
+                    {
+                        dt.Rows[rowId][columnId] = 0;
+                    }
+                    else
+                    {
+                        dt.Rows[rowId][columnId] = values[index];
+                    }                    
+                    index++;
+                }                
+            }            
+            dataGrid1.DataContext = dt;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            udpConnection.Discover();
+            //TextBox_BytesString.Text.Insert(TextBox_BytesString.MaxLength, discoveryData[28].ToString() + discoveryData[29] + discoveryData[30]);
+            //byte TOD1_1 = discoveryData[28];
+            //byte TOD1_2 = discoveryData[29];
+            //byte TOD1_3 = discoveryData[30];
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _port = int.Parse(TextBox_Port.Text);
+            udpConnection.ConnectToDevice(TextBox_IP.Text, _port);
+            Label_IP.Content = TextBox_IP.Text;
+            Label_PORT.Content = _port.ToString();
+        }
+
+        public void updateReceiveMessage()
+        {
+            TextBox_BytesString.Text = udpConnection.message;
+        }
+    }
+}
